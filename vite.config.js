55d7d3a25389eaefe74145d7db54cf6253e1020f
benjjiu04 <benjaminvargas2004@hotmail.com>
2026-06-11 19:13:13 -0300

    ando chato de esto

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,15 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+
+  base: '/proyecto-frontend/', 
   
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+
+  
   
 //necesario para Linux o el subsistema WSL para que se actualice constantemente la pagina.
  server: {
@@ -18,3 +22,4 @@ export default defineConfig({
   }
  } 
 })
+
